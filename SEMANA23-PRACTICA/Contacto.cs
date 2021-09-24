a818ff08b55031a34bec103daab0cb0d03a31994
Yosselin Flores <yoshy.floresg17@gmail.com>
2021-09-23 21:03:37 -1100

    Agregar archivos de proyecto.

---
+++ SEMANA23-PRACTICA/Contacto.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA23_PRACTICA
+{
+
+    class Contacto
+    {
+        //Definir miembros de la clase
+        //propiedades|atributos| datos.
+
+        //normalmente las propiedades se definen en public
+
+        //cuando sea de un elemento de herencia o padre se establecera como privado
+
+        private int _contactoId;
+        private string _nombre;
+        private int _contactoCasa_Trabajo;
+        private string _empresaT;
+
+        //metodos de propiedad
+        //get--obtinene el valor almacenado en la propiedad
+        //set-- asigna un valor a la variable de propiedad
+
+        //contactoId
+        public void setContactoId(int contactoId)
+        {
+            _contactoId = contactoId;
+
+        }
+        public int getContactoId()
+        {
+            return _contactoId;
+        }
+        public void setnombre(string nombre)
+        {
+            _nombre = nombre;
+        }
+        public string getNombre()
+        {
+            return _nombre;
+        }
+
+        public void setContactoCasa_Trabajo(int contactoCasa_Trabajo)
+        {
+            _contactoCasa_Trabajo = contactoCasa_Trabajo;
+        }
+
+        public int getContactoCasa_Trabajo()
+        {
+            return _contactoCasa_Trabajo;
+        }
+        public void setEmpresaT(string empresaT)
+        {
+            _empresaT = empresaT;
+        }
+        public string getEmpresaT()
+        {
+            return _empresaT;
+        }
+    }
+}
---
+++ SEMANA23-PRACTICA/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SEMANA23_PRACTICA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //instacia de la clase contacto
+            //cuando creo una instacia debo hace referencia ala clase
+
+            Contacto contacto = new Contacto();
+
+            //mediante metodos de propiedad
+            //creamos un objeto contacto
+            contacto.setContactoId(60020601);
+            contacto.setnombre("Juan Enrique");
+            contacto.setContactoCasa_Trabajo(25250025);
+            contacto.setEmpresaT("Naciones Unidas");
+
+
+            //mostrar datos de contacto
+            MessageBox.Show(contacto.getContactoId() + "  " + contacto.getNombre() + "  "+ contacto.getContactoCasa_Trabajo() + "  "+ contacto.getEmpresaT());
+
+            //mediante propiedad autoimplementadacontacto.
+            //SET
+            //contacto._publishedEmpresaT= "Naciones Unidad";
+            //GET
+            //MessageBox.Show(book._publishedEmpresaT);
+        }
+    }
+}
